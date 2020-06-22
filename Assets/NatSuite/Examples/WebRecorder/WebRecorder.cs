10c398247440b391e4ea6a92542e351777537c9f
Yusuf Olokoba <lanre@krikey.com>
2020-06-22 12:05:15 -0400

    Implement web recorder example

--- Assets/NatSuite/Examples/WebRecorder/WebRecorder.cs
+++ Assets/NatSuite/Examples/WebRecorder/WebRecorder.cs
@@ -6,6 +6,8 @@
 namespace NatSuite.Examples {
 
     using UnityEngine;
+    using UnityEngine.UI;
+    using System.Collections;
     using Recorders;
     using Recorders.Inputs;
 
@@ -15,8 +17,25 @@ namespace NatSuite.Examples {
         public int videoWidth = 1280;
         public int videoHeight = 720;
 
+        [Header(@"UI")]
+        public RawImage rawImage;
+        public AspectRatioFitter aspectFitter;
+
         private WEBMRecorder recorder;
         private ScreenInput screenInput;
+
+        IEnumerator Start () {
+            // Request camera permission
+            yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+            // Start webcam
+            var webCamTexture = new WebCamTexture();
+            webCamTexture.Play();
+            // Weird macOS bug
+            yield return new WaitUntil(() => webCamTexture.width > 16 && webCamTexture.height > 16);
+            // Display UI
+            rawImage.texture = webCamTexture;
+            aspectFitter.aspectRatio = (float)webCamTexture.width / webCamTexture.height;
+        }
 
         public void StartRecording () {
             // Start recording from main camera
